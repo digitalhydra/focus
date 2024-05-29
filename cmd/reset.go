1a0f62222bd88399b81c05c95d634d44568f6cf2
digitalhydra <jimnoname@gmail.com>
2024-05-29 17:24:40 -0500

    add default config file if none specified or not found

---
+++ cmd/reset.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// resetCmd represents the reset command
+var resetCmd = &cobra.Command{
+	Use:   "reset",
+	Short: "Reset config to default values! CAUTION current schedule will be lost",
+	Long:  `for testing or not, use with caution`,
+	Run: func(cmd *cobra.Command, args []string) {
+		createDefaultConfigFile()
+		fmt.Println("Config reseted")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(resetCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// resetCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// resetCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ Copyright © 2024 focus
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	breaker "focus/cmd/break"
 	exception "focus/cmd/exception"
@@ -20,13 +21,8 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "focus",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A tool to set a schedule to focus, a CLI tool for ADHD",
+	Long:  `Once a schedule is setup a service will stop apps and urls from running so you can focus`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -48,7 +44,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.focus.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.config/.focus.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
@@ -57,18 +53,22 @@ func init() {
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
+	fmt.Print("initconfig ")
 	if cfgFile != "" {
+		fmt.Printf("cfgfile!=empty")
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
+		fmt.Printf(" else setup viper")
 		// Find home directory.
-		home, err := os.UserHomeDir()
+		configPath, err := os.UserConfigDir()
 		cobra.CheckErr(err)
 
 		// Search config in home directory with name ".focus" (without extension).
-		viper.AddConfigPath(home)
+		viper.AddConfigPath(configPath)
 		viper.SetConfigType("yaml")
 		viper.SetConfigName(".focus")
+
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
@@ -76,6 +76,9 @@ func initConfig() {
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	} else {
+		fmt.Printf(" else config")
+		createDefaultConfigFile()
 	}
 }
 
@@ -84,4 +87,32 @@ func addSubcommandPalletes() {
 	rootCmd.AddCommand(service.ServiceCmd)
 	rootCmd.AddCommand(exception.ExceptionCmd)
 	rootCmd.AddCommand(breaker.BreakCmd)
+}
+
+func createDefaultConfigFile() {
+
+	var defaultConfig = []byte(`
+startdate: 01/01/2024
+enddate: null
+weekdays:
+- monday
+- tuesday
+- wednesday
+- thursday
+- friday
+apps:
+- leather
+- denim
+starttime: 8:00am
+endtime: 4:00pm
+onbreak: false
+exception: 
+- 15/05/2024
+`)
+	viper.ReadConfig(bytes.NewBuffer(defaultConfig))
+	err := viper.SafeWriteConfig()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "set default config file:", viper.ConfigFileUsed())
+	}
+
 }
